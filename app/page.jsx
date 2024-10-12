6071506e3b1173ef12484921498c2c2606c8ded6
AlanGcDev <gamebrocoli@gmail.com>
2024-10-12 12:41:32 -0600

    s

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
-            <h1> f en el chat</h1>
+            <h1> f en el chat<Icon icon="mdi:home" /></h1>
         </main>
     );
 }
