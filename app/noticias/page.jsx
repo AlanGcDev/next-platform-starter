d5d4a1faf14f45e461319ad465643dfcee05421e
AlanGcDev <gamebrocoli@gmail.com>
2024-10-13 12:00:34 -0600

    as

--- app/noticias/page.jsx
+++ app/noticias/page.jsx
@@ -1,34 +1,25 @@
-"use client"; // Asegúrate de ejecutar solo en el cliente
-
 import { Icon } from '@iconify/react';
-import dynamic from 'next/dynamic'; // Para carga dinámica del componente Ads
+import { Ads } from 'components/ads';
 import Link from 'next/link';
-
-// Cargar el componente Ads sin SSR
-const Ads = dynamic(() => import('components/ads'), { ssr: false });
 
 export default function Page() {
     return (
+
         <div className="container">
             <div className="content">
                 <Link href="/noticias/tecnologia/inteligencia-artificial" className="content-article">
                     <div className="content-article-img">
-                        {/* Usa imágenes optimizadas con Next.js */}
-                        <img 
-                            src="https://example.com/image.jpg" 
-                            alt="Image Description" 
-                            loading="lazy" // Carga diferida para mejorar el rendimiento
-                        />
+                        <img src="https://example.com/image.jpg" alt="Image Description" />
                     </div>
                     <div className="content-article-text">
                         <h2>Title</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel justo sed nunc condimentum convallis et ac eros.</p>
                     </div>    
                 </Link>
+                
             </div>
-
-            {/* Renderizar anuncios dinámicamente */}
-            <Ads />
+            <Ads></Ads>
         </div>
+        
     );
 }
